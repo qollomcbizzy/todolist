b995aad3db10c3a19bf6814c4c2982f26ffbeb01
collo <qollomcbizzy64@gmail.com>
2017-05-09 11:16:05 +0300

    added the second  specification

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ KIPROTICH COLLINS
 this project indicates a list of task that somebody wants to do
 #### PROJECT DESCRIPTION
 1. Create a back-end interface with the properties of object task;
+2. Create the html and the user interface to receive inputted values
--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-//back-end logic 
+//back-end logic
 function Task(date,time,location,notes){
   this.taskDate=date;
   this.taskTime=time;
   this.taskLoaction=location;
   this.taskNotes=notes;
 };
+//user interface
+$(document).ready(function(){
+  $("form#todolist").submit(function(){
+    var inputtedTask=$("textarea#new-task").val();
+      var inputtedLocation=$("input#new-location").val();
+        var inputtedDate=$("input#new-date").val();
+          var inputtedTime=$("input#new-time").val();
+            var inputtedNotes=$("textarea#new-notes").val();
+  });
+});
