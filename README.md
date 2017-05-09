32a12aa82b274c5ea6789a3648eee21999601a47
collo <qollomcbizzy64@gmail.com>
2017-05-09 14:23:23 +0300

    added the fourth  specification in addition to checkbox

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ this project indicates a list of task that somebody wants to do
 #### PROJECT DESCRIPTION
 1. Create a back-end interface with the properties of object task;
 2. Create the html and the user interface to receive inputted values
-3. create a copy constructor 
+3. create a copy constructor
+4. create div to show the list when the user clicks
--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 //back-end logic
-function Task(date,time,location,notes){
+function Task(task,location,date,time){
+  this.taskName=task;
   this.taskDate=date;
   this.taskTime=time;
-  this.taskLoaction=location;
-  this.taskNotes=notes;
+  this.taskLocation=location;
 };
 //user interface
 $(document).ready(function(){
-  $("form#todolist").submit(function(){
+  $("form#todolist").submit(function(event){
+    event.preventDefault();
     var inputtedTask=$("textarea#new-task").val();
       var inputtedLocation=$("input#new-location").val();
         var inputtedDate=$("input#new-date").val();
           var inputtedTime=$("input#new-time").val();
-            var inputtedNotes=$("textarea#new-notes").val();
-    var newTask=new Task(inputtedTask,inputtedLocation,inputtedDate,inputtedTime,inputtedNotes);
+    var newTask=new Task(inputtedTask,inputtedLocation,inputtedDate,inputtedTime);
+    //show places
+    $("ul#task").append("<li><span class='task'>" + newTask.taskName + "</span></p>");
+    $(".task").click(function(){
+      $("#show-details").show();
+      $(".task").text(newTask.taskName);
+      $(".location").text(newTask.taskLocation);
+      $(".date").text(newTask.taskDate);
+      $(".time").text(newTask.taskTime);
+    });
+    $("textarea#new-task").val();
+    $("input#new-location").val();
+      $("input#new-date").val();
+        $("input#new-time").val();
   });
 });
